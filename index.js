4186c59c6f517594aad23d288da439ff4dd8acd8
Soyas Limbu <90306699+soyas07@users.noreply.github.com>
2022-01-31 23:14:26 +1100

    Memories app Deployment

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import dotEnv from 'dotenv'
+import dotEnv from 'dotenv/config'
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
-dotEnv.config()
 const db = process.env.DB_URL // db url
 const port = process.env.PORT || 5000
 
@@ -19,6 +18,10 @@ app.use(cors())
 
 // post routes
 app.use('/posts', postRoutes) 
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memmories')
+})
 
 // connect to db
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
